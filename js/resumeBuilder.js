9f2d65c704f6147d4ed8a73c212a538b472e4f2b
George Keith <ggkeithv@gmail.com>
2016-04-24 00:53:12 -0500

    added work experience and biography

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -18,12 +18,54 @@ var skills = ["Javascript", "jQuery", "HTML", "CSS", "NodeJS", "AngularJS"];
 var bio = {
   "name": "George Keith",
   "role": "Web Developer",
-  "contact-info": {
+  "contact": {
     "phone": "(214)205-7336",
     "email": "george@awkwardlybrilliant.com",
     "github": "ggkeith",
-    "website": "www.awkwardlybrilliant.com"
+    "twitter": "@ggkeith",
+    "location": "Dallas, TX"
   },
   "message": "I am currently studying to be a full-stack web developer.",
-  "skills": skills
+  "skills": skills,
+  "bioPic": "images/fry.jpg"
 };
+
+// variables for formatted contacts
+var formattedPhone = HTMLmobile.replace("%data%", bio.contact.phone);
+var formattedEmail = HTMLemail.replace("%data%", bio.contact.email);
+var formattedLocation = HTMLlocation.replace("%data%", bio.contact.location);
+var formattedGithub = HTMLgithub.replace("%data%", bio.contact.github);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
+var formattedPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+
+//append contact info to site
+$(".flex-box").append(formattedPhone);
+$(".flex-box").append(formattedEmail);
+$(".flex-box").append(formattedLocation);
+$(".flex-box").append(formattedGithub);
+$(".flex-box").append(formattedTwitter);
+$("#topContacts").prepend(formattedPic);
+
+
+var work = {};
+var education = {};
+
+work.position = "Director of Client Services";
+work.employer = "AutoRevo";
+work.years = "2014-present";
+work.city = "Plano, TX";
+
+education["school"] = "freeCodeCamp";
+education["years"] = "2015-2016";
+education["city"] = "Dallas, TX";
+
+var formattedEmployer = HTMLworkEmployer.replace("%data%", work["employer"]);
+var formattedTitle = HTMLworkTitle.replace("%data%", work["position"]);
+var formattedDates = HTMLworkDates.replace("%data%", work["years"]);
+
+$("#workExperience").append(HTMLworkStart);
+$(".work-entry").append(formattedEmployer);
+$(".work-entry").append(formattedTitle);
+$(".work-entry").append(formattedDates);
+//$("").append();
